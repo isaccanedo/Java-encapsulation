24bd15027b818e73ffcd67b03ec22aa1780322e8
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-01-12 14:04:15 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 
 public class Main {
 	
+	/* +---------------------------------------------
+	 * | @author Isac Canedo
+	 * +---------------------------------------------
+	 */
+	
 	/*
 	 * By implementing encapsulation, you cannot directly access the class's ______ properties unless you are writing 
 	 * code inside the class itself.
 	 * 
-	 * Ao implementar o encapsulamento, você não pode acessar diretamente as propriedades ______ da classe, 
-	 * a menos que esteja escrevendo o código dentro da classe.
+	 * Ao implementar o encapsulamento, vocÃª nÃ£o pode acessar diretamente as propriedades ______ da classe, 
+	 * a menos que esteja escrevendo o cÃ³digo dentro da classe.
 	 * 
 	 * 1 - private == correct
 	 * 2 - public
