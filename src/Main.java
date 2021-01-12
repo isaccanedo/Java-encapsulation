4874a391fb29ad60b555dfeaf58f7e8a222f05be
isaccanedo <isaccanedo@gmail.com>
2021-01-12 12:07:25 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	
+	/*
+	 * By implementing encapsulation, you cannot directly access the class's ______ properties unless you are writing 
+	 * code inside the class itself.
+	 * 
+	 * Ao implementar o encapsulamento, você não pode acessar diretamente as propriedades ______ da classe, 
+	 * a menos que esteja escrevendo o código dentro da classe.
+	 * 
+	 * 1 - private == correct
+	 * 2 - public
+	 * 3 - protected
+	 * 4 - no-modifier
+	 * 
+	 */		
+
+	public static void main(String[] args) {		
+		
+		Teste teste = new Teste();
+		teste.correr2();
+		teste.correr3();		
+		
+	}
+}
---
+++ src/Teste.java
@@ -0,0 +1,17 @@
+
+public class Teste {
+	
+	private void correr() {
+		System.out.println("Correndo!");
+	}
+	
+	public void correr2() {
+		System.out.println("Correndo 2!");
+	}
+	
+	protected void correr3() {
+		
+		System.out.println("correndo 3!");
+	}
+
+}
